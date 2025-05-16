4f3ca04bc2771f997e413c31bdb30bbbdbb6ff3e
11YuKING18 <s1f102500427@toyo.jp>
2025-05-17 02:26:48 +0900

    マインスイーパ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,61 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './page.module.css';
+const calcTotalPoint = (arr: number[], counter: number) => {
+  let result = 0;
+  for (const item of arr) {
+    result += item;
+  }
+  return result + counter;
+};
+const down = (n: number) => {
+  if (n < 0) {
+    return down;
+  } else {
+    console.log(n);
+    down(n - 1);
+  }
+};
+down(10);
+const sum1 = (n: number): number => {
+  if (n === 0) return 0;
+  return n + sum1(n - 1);
+}; /*今nに10が入っていて、もしnが0だったらreturn(終了)nが0じゃないから次の行に進んで、
+10+9が実行されて、関数内に同じ関数名が出現しているから再起関数でまた最初に戻る。if文は1行の時{}は省略できる、
+elseは使っても使わなくてもいい。22行～23行目までが合計を出すのに最短ルート*/
+console.log(sum1(10));
 
+const sum2 = (a: number, b: number): number => {
+  return a === b + 1 ? 0 : a + sum2(a + 1, b); //三項演算子
+};
+console.log(sum2(4, 10));
+
+const sum3 = (c: number, d: number): number => {
+  return ((d - c + 1) / 2) * (c + d);
+};
+console.log(sum3(4, 10));
 export default function Home() {
+  const [samplePoints, setsamplePoints] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  console.log(samplePoints);
+  const [sampleCounter, setsampleCounter] = useState(0);
+  console.log(sampleCounter);
+  const clickHandler = () => {
+    const newSamplePoints = structuredClone(samplePoints);
+    newSamplePoints[sampleCounter] += 1;
+    setsamplePoints(newSamplePoints);
+    setsampleCounter((sampleCounter + 1) % 14);
+    //samplecounterは定数、setsamplecounterになると数値を更新して値が変わる
+  };
+  const totalPoint = calcTotalPoint(samplePoints, sampleCounter);
+  console.log(totalPoint);
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            src/app/page.tsx
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div
+        className={styles.samplecell}
+        style={{ backgroundPosition: `${sampleCounter * -30}px` }}
+      />
+      <button onClick={clickHandler}>クリック</button>
     </div>
   );
 }
